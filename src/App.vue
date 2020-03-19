ab9cb64a9902f1058396076bd8bdfcea36a46c7f
pakpoom <pakpoom.t@student.chula.ac.th>
2020-03-19 23:25:14 +0700

    implement AppHeader layout

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/layout/AppHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <span class="logo">Little Brown Book Shop</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped lang="scss">
+  .header {
+    position: fixed;
+    top: 0px;
+    width: 100%;
+    height: 64px;
+    z-index: 9999;
+    background-color: #C8A58C;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .logo {
+      color: white;
+      font-size: 24px;
+      font-weight: bold;
+    }
+  }
+</style>
