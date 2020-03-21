3a2a5e95d191818c859534770d1dd4702ab0df13
pakpoom <pakpoom.t@student.chula.ac.th>
2020-03-21 11:08:34 +0700

    register ant-design grid component to be globally

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { 
+  Col,
+  Row,
+} from 'ant-design-vue';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// Registration for global components
+Vue.component('Col', Col);
+Vue.component('Row', Row);
