0ba09f1f5ac722f73a9cfa2f2d36af1bb3b78e64
pakpoom <pakpoom.t@student.chula.ac.th>
2020-03-22 22:46:21 +0700

    TotalCost accepts prop to display cash change

--- src/pages/PointOfSalePage/Cart/TotalCost.vue
+++ src/pages/PointOfSalePage/Cart/TotalCost.vue
@@ -28,19 +28,63 @@
         {{ totalCost }} Baht
       </Col>
     </Row>
+    <template v-if="isChangeEnabled">
+      <Row>
+        <Col
+          :span="18"
+          class="row-title"
+        >
+          Cash:
+        </Col>
+        <Col
+          :span="6"
+          class="row-value"
+        >
+          {{ receivedCash }} Baht
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          :span="18"
+          class="row-title"
+        >
+          Change:
+        </Col>
+        <Col
+          :span="6"
+          class="row-value"
+        >
+          {{ changeCash }} Baht
+        </Col>
+      </Row>
+    </template>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { toNumber } from 'lodash';
+import {
+  mapGetters,
+  mapState,
+} from 'vuex';
 
 export default {
   name: 'TotalCost',
+  props: {
+    isChangeEnabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
   computed: {
+    changeCash: function () {
+      return this.receivedCash - toNumber(this.totalCost);
+    },
     ...mapGetters('cart', [
       'totalDiscount',
       'totalCost',
     ]),
+    ...mapState('payment', ['receivedCash']),
   },
 };
 </script>
