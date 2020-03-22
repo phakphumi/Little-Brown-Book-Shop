2b57845d31634d48489a96b61f7d20df3f64ddb1
pakpoom <pakpoom.t@student.chula.ac.th>
2020-03-22 18:17:26 +0700

    modify ant theme for primary color

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          'primary-color': '#00410F',
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+};
