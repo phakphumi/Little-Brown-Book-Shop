07cd63f70a63366d09eca4ae59ba48958af4b405
pakpoom <pakpoom.t@student.chula.ac.th>
2020-03-23 01:39:31 +0700

    setup pre-styling

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   css: {
     loaderOptions: {
+      sass: {
+        prependData: '@import "@/styles/index.scss";',
+      },
       less: {
         modifyVars: {
           'primary-color': '#00410F',
