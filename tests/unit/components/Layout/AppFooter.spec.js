cbf1e7181c8c45926af491b5a446cf191dd3114c
pakpoom <pakpoom.t@student.chula.ac.th>
2020-03-23 15:33:21 +0700

    remove unused component

--- tests/unit/components/Layout/AppFooter.spec.js
+++ tests/unit/components/Layout/AppFooter.spec.js
@@ -3,7 +3,7 @@ import { shallowMount } from '@vue/test-utils';
 import AppFooter from '@/components/Layout/AppFooter';
 
 describe('App.vue', () => {
-  it('must logo class should contain Little Brown Book Shop typo', () => {
+  it('must contains © LITTLE BROWN BOOK SHOP typo', () => {
     const wrapper = shallowMount(AppFooter, { stubs: ['Col', 'Row'] });
     expect(wrapper.text()).toContain('© LITTLE BROWN BOOK SHOP');
   });
